f8248d7dbddb8217a6471a0449a4b64de9738755
Eduardo <9207448@alumnos.ufv.es>
2025-01-05 09:02:39 +0100

    Added multiplication

--- src/Main.java
+++ src/Main.java
@@ -42,6 +42,12 @@ public class Main {
                     break;
                 case 3:
                     System.out.println("Seleccionó multiplicar");
+                    System.out.println("Ingrese el primer número: ");
+                    int e = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Ingrese el segundo número: ");
+                    int f = Integer.parseInt(scanner.nextLine());
+                    System.out.println("El resultado de la multiplicación es: " + operations.multiply(e, f));
+                    menu();
                     break;
                 case 4:
                     System.out.println("Seleccionó dividir");
